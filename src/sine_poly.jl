5b53795db3ae4f3b913399bb7c87ee3be17c9a64
Ed Scheinerman <ers@jhu.edu>
2018-11-02 14:00:15 -0400

    update

---
+++ src/sine_poly.jl
@@ -0,0 +1,22 @@
+using Polynomials, BigCombinatorics
+
+
+function sine_coeffs(n::Int)
+    T = Rational{BigInt}
+    result = zeros(T,2n)
+    for j=1:n
+        result[2j] = (-1)^(j-1)//Factorial(2j-1)
+    end
+
+
+    return result
+end
+
+
+"""
+`sine_poly(n)` create exact sine power series of degree `2n`
+(actually `2n-1` because `sin` is odd).
+"""
+function sine_poly(n::Int)
+    return Poly(sine_coeffs(n))
+end
