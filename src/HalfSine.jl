753a496590d467ee5ae648a67b5597dd7cd7bc2f
Ed Scheinerman <ers@jhu.edu>
2020-10-19 08:27:06 -0400

    extend semisine

--- src/HalfSine.jl
+++ src/HalfSine.jl
@@ -17,6 +17,7 @@ a = [ b[k]//big(2)^(k-1) // Factorial(2k-1) for k=1:ndata ]
 value of the half sine.
 """
 function semisine(x::Real,terms=10)
+    x = _reset(x)
     sum(  a[k]*x^(2k-1) for k=1:terms)
 end
 
