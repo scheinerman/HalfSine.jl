79326082833c1da01ed20109260effb34c568a98
Ed Scheinerman <ers@jhu.edu>
2020-10-19 07:31:10 -0400

    Updates

--- src/HalfSine.jl
+++ src/HalfSine.jl
@@ -12,10 +12,15 @@ include("data.txt")  # get the b-values
 ndata = length(b)
 a = [ b[k]//big(2)^(k-1) // Factorial(2k-1) for k=1:ndata ]
 
-
+"""
+`semisine(x,terms=10)` gives the analytic (power series)
+value of the half sine.
+"""
 function semisine(x::Real,terms=10)
     sum(  a[k]*x^(2k-1) for k=1:terms)
 end
+
+include("rufus.jl")
 
 
 end
--- src/rufus.jl
+++ src/rufus.jl
@@ -1,39 +1,38 @@
-using SimpleDrawing, Plots
+export hs0, hs
 
-
-
-f0(x) = (x>=0) ? -x : sin(-x)
+"""
+`hs0(x)` returns the Rufus Isaacs derived half sine
+that is valid on [-π,π].
+"""
+hs0(x) = (x>=0) ? -x : sin(-x)
 
 
 
 """
-`hs(x)` is a continuous half-sine function.
+`hs(x)` is a continuous half-sine function created by extending
+`hs0`.
 """
 function hs(x::T) where T<:Real
-    x = reset(x)
-    return f0(x)
+    x = _reset(x)
+    return hs0(x)
 end 
 
-function reset(x::T) where T<:Real
+
+"""
+`_reset(x)` is `asin(sin(x))` to map `x` 
+into the domain of `h0`.
+"""
+function _reset(x::T) where T<:Real
     x = asin(sin(x))
 end
 
-hs2(x) = hs(hs(x))
-
-
-function coord_axes(x1,x2,y1,y2)
-    draw_vector(x1,0,color=:black,linewidth=0.5)
-    draw_vector(x2,0,color=:black,linewidth=0.5)
-    draw_vector(0,y1,color=:black,linewidth=0.5)
-    draw_vector(0,y2,color=:black,linewidth=0.5)
-end
 
 function make_picture()
     plot(hs,-4pi,4pi,aspect_ratio=1,legend=false,color=:black)
-
-    coord_axes(-4.25pi,4.25pi,-2,1.5)
+    draw_xaxis()
+    draw_yaxis()
+    
     finish()
-    #plot!(hs2,-4pi,4pi,color=:red)
 end
 
 function make_picture_1()
@@ -46,8 +45,11 @@ function make_picture_1()
     draw_vector(0,1.2,color=:black,linewidth=0.5)
     draw_vector(0,-1.7,color=:black,linewidth=0.5)
 
-    draw_segment(-pi/2,-0.1,-pi/2,0.1,color=:black,linewidth=0.5)
-    draw_segment(pi/2,-0.1,pi/2,0.1,color=:black,linewidth=0.5)
+    # draw_segment(-pi/2,-0.1,-pi/2,0.1,color=:black,linewidth=0.5)
+    # draw_segment(pi/2,-0.1,pi/2,0.1,color=:black,linewidth=0.5)
+
+    draw_xaxis()
+    draw_yaxis()
 
     finish()
 end
